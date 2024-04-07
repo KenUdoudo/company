0a17d16844d7e2f559d2cce940f61758be19f5ac
Ken Udoudo <kendo302003@gmail.com>
2024-04-07 12:58:12 -0700

    sec1

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
     <div className='px-[50px]'>
       <div className='flex flex-row justify-between items-center bg-gray-100 rounded-full px-[10px] py-[20px] mt-[10px]'>
         <div className="flex flex-row justify-center items-center gap-x-[15px]">
-          <div className="h-[30px] w-[30px] rounded-full bg-green-500"></div>
+          <div className="h-[30px] w-[30px] rounded-full bg-green-300"></div>
           <button className={`${tab === 0 ? " bg-gray-200" : ""} border-[1px] rounded-full px-[15px] py-[3px] font-roboto text-[16px] font-[500]`} onClick={()=> setTab(0)}>Home</button>
           <button className={`${tab === 1 ? " bg-gray-200" : ""} border-[1px] rounded-full px-[15px] py-[3px] font-roboto text-[16px] font-[500]`} onClick={()=> setTab(1)}>Get involved</button>
           <button className={`${tab === 2 ? " bg-gray-200" : ""} border-[1px] rounded-full px-[15px] py-[3px] font-roboto text-[16px] font-[500]`} onClick={()=> setTab(2)}>Donate</button>
@@ -18,9 +18,9 @@ function App() {
           <button className={`${tab === 4 ? " bg-gray-200" : ""} border-[1px] rounded-full px-[15px] py-[3px] font-roboto text-[16px] font-[500]`} onClick={()=> setTab(4)}>Success Stories</button>
           <button className={`${tab === 5 ? " bg-gray-200" : ""} border-[1px] rounded-full px-[15px] py-[3px] font-roboto text-[16px] font-[500]`} onClick={()=> setTab(5)}>News</button>
         </div>
-        <div className='flex flex-row justify-center items-center gap-[4px] rounded-full px-[10px] py-[3px] bg-green-500 cursor-pointer'>
+        <div className='flex flex-row justify-center items-center gap-[4px] rounded-full px-[10px] py-[3px] bg-green-300 cursor-pointer'>
           <p className='text-[13px] font-roboto font-[500]'>Contact Us</p>
-          <div className='h-[25px] w-[25px] rounded-full border-[1px] bg-green-200'></div>
+          <div className='h-[25px] w-[25px] rounded-full border-[1px] bg-green-100'></div>
         </div>
       </div>
 
--- src/components/Home/section1.jsx
+++ src/components/Home/section1.jsx
@@ -1,11 +1,11 @@
 const Section1 = () => {
     return (
-        <div>
-            <p>Transforming Lives Through <br /> Hope and Education</p>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur repudiandae numquam autem nostrum dolor maiores sapiente eaque aliquam adipisci nam quam soluta expedita dicta, voluptate vel at nisi dolores voluptates.</p>
-            <div className='flex flex-row justify-center items-center gap-[4px] rounded-full px-[10px] py-[3px] bg-green-500 cursor-pointer'>
+        <div className="flex flex-col justify-center items-center text-center gap-y-[20px]">
+            <p className="text-[38px] font-[600]">Transforming Lives Through <br /> Hope and Education</p>
+            <p className="px-[80px] text-[14px] font-inter font-[500]">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur repudiandae numquam autem nostrum dolor maiores sapiente eaque aliquam adipisci nam quam soluta expedita dicta, voluptate vel at nisi dolores voluptates.</p>
+            <div className='flex flex-row justify-center items-center gap-[4px] rounded-full px-[10px] py-[3px] bg-green-300 cursor-pointer'>
                 <p className='text-[13px] font-roboto font-[500]'>Donate Now</p>
-                <div className='h-[25px] w-[25px] rounded-full border-[1px] bg-green-200'></div>
+                <div className='h-[25px] w-[25px] rounded-full border-[1px] bg-green-100'></div>
             </div>
         </div>
     );
