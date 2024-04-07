f5dac52eb3544f01fcdc373333ecb1eeff3184e8
Ken Udoudo <kendo302003@gmail.com>
2024-04-07 12:00:30 -0700

    css

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import { useState } from 'react'
 
 function App() {
-
+  const [tab, setTab] = useState(0);
   return (
     <div>
       <div>
-        <div className="flex flex-row justify-between items-center">
-          <div className="h-[30px] w-[30px] border"></div>
+        <div className="flex flex-row justify-around items-center">
+          <div className="h-[30px] w-[30px] rounded-full bg-green-500"></div>
+          <button className={`${tab === 0 ? " bg-gray-300" : ""} border-[1px] px-[3px] py-[1px] font-roboto text-[16px]`} onClick={()=> setTab(0)}>Home</button>
         
 
         </div>
