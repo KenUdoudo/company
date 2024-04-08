2258d3382845cb6976e275c28d5491bb1c6e13b8
Ken Udoudo <kendo302003@gmail.com>
2024-04-08 12:45:32 -0700

    importing section2

---
+++ src/components/Home/section2.jsx
@@ -0,0 +1,7 @@
+const Section2 = () => {
+    return (
+        <div></div>
+    );
+}
+ 
+export default Section2;
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,9 +1,12 @@
 import Section1 from "../components/Home/section1";
+import Section2 from "../components/Home/section2";
 
 const Home = () => {
     return (
         <div>
             <Section1 />
+
+            <Section2 />
         </div>
     );
 }
