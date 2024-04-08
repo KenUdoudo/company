9f443aab6d4d62e69b214c6d3fc0bc25be6bf17b
Ken Udoudo <kendo302003@gmail.com>
2024-04-08 12:53:50 -0700

    fuck

--- src/components/Home/section2.jsx
+++ src/components/Home/section2.jsx
@@ -1,6 +1,26 @@
 const Section2 = () => {
     return (
-        <div></div>
+        <div>
+            <div className="text-center">
+                <p className="text-[28px] font-semibold">Give a Child Hope Today</p>
+                <p className="text-[13px]">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed, commodi architecto laborum illum incidunt voluptatem. </p>
+            </div>
+
+            <div>
+                <div>
+                    <p>85%</p>
+                    <p className="text-[13px]">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestiae quis numquam dignissimos aliquam debitis cum magnam suscipit!</p>
+                </div>
+                <div>
+                    <p>3,250</p>
+                    <p className="text-[13px]">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestiae quis numquam dignissimos aliquam debitis cum magnam suscipit!</p>
+                </div>
+                <div>
+                    <p>69%</p>
+                    <p className="text-[13px]">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestiae quis numquam dignissimos aliquam debitis cum magnam suscipit!</p>
+                </div>
+            </div>
+        </div>
     );
 }
  
