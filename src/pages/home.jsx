8c3b5eb929d009ebdf94cab2c369359b081cb4fc
Ken Udoudo <kendo302003@gmail.com>
2024-04-09 09:44:07 -0700

    importing sec 4

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,6 +1,7 @@
 import Section1 from "../components/Home/section1";
 import Section2 from "../components/Home/section2";
 import Section3 from "../components/Home/section3";
+import Section4 from "../components/Home/section4";
 
 const Home = () => {
     return (
@@ -10,6 +11,8 @@ const Home = () => {
             <Section2 />
 
             <Section3 />
+
+            <Section4 />
         </div>
     );
 }
