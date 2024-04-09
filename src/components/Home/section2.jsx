35c02ad51350fcea38edb25b340127c13f6d9265
Ken Udoudo <kendo302003@gmail.com>
2024-04-09 08:39:42 -0700

    first bit

--- src/components/Home/section2.jsx
+++ src/components/Home/section2.jsx
@@ -7,15 +7,15 @@ const Section2 = () => {
             </div>
 
             <div className="flex flex-row justify-center items-center px-[50px]">
-                <div className="border-[2px] rounded-l-[15px]">
-                    <p>85%</p>
+                <div className="border-[2px] rounded-l-[15px] flex flex-col justify-center items-center text-center bg-gray-100 gap-[5px] px-[20px] py-[20px]">
+                    <p className="text-[28px] font-semibold bg-gray-200 px-[15px] rounded-md">85%</p>
                     <p className="text-[13px]">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestiae quis numquam dignissimos aliquam debitis cum magnam suscipit!</p>
                 </div>
-                <div>
+                <div className="border-[2px]">
                     <p>3,250</p>
                     <p className="text-[13px]">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestiae quis numquam dignissimos aliquam debitis cum magnam suscipit!</p>
                 </div>
-                <div>
+                <div className="border-[2px] rounded-r-[15px]">
                     <p>69%</p>
                     <p className="text-[13px]">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestiae quis numquam dignissimos aliquam debitis cum magnam suscipit!</p>
                 </div>
