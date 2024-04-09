9f0b191031f567749893a5f0f3a2c7ce7d7697c3
Ken Udoudo <kendo302003@gmail.com>
2024-04-09 09:38:34 -0700

    done done

--- src/components/Home/section3.jsx
+++ src/components/Home/section3.jsx
@@ -39,6 +39,38 @@ const Section3 = () => {
                     </div>
                 </div>
             </div>
+            <div className="flex flex-row-reverse justify-center items-center px-[50px] gap-[30px] mt-[30px]">
+                <div className="w-[40%] bg-[#dfee9b] rounded-lg border-[5px] h-[440px]"></div>
+                <div className="w-[60%] bg-gray-200 p-[30px] rounded-lg flex flex-col justify-center space-y-[30px]">
+                    <div className="flex flex-row justify-between items-center">
+                        <p className="font-semibold text-[20px]">Vision</p>
+                        <div className='flex flex-row justify-center items-center gap-[4px] rounded-full bg-white px-[13px] py-[10px] cursor-pointer'>
+                            <p className='text-[13px] font-roboto font-[500]'>Learn More</p>
+                            <HiArrowUpRight/>
+                        </div>
+                    </div>
+
+                    <div className="flex flex-col justify-center items-center bg-white rounded-lg p-[20px]">
+                        <div className="flex flex-row gap-[10px] justify-normal items-center border-b-[2px] pb-[20px]">
+                            <div className="h-[40px] w-[40px] bg-[#CEFA05] rounded-full text-[20px] font-semibold flex justify-center items-center shrink-0">01</div>
+                            <p className="text-[13px]">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias nemo aperiam, deleniti nobis eius maxime, est reprehenderit eaque modi rerum excepturi laudantium,</p>
+                        </div>
+                        <div className="flex flex-row gap-[10px] justify-normal items-center border-b-[2px] pt-[15px] pb-[20px]">
+                            <div className="h-[40px] w-[40px] bg-[#CEFA05] rounded-full text-[20px] font-semibold flex justify-center items-center shrink-0">02</div>
+                            <p className="text-[13px]">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias nemo aperiam, deleniti nobis eius maxime, est reprehenderit eaque modi rerum excepturi laudantium,</p>
+                        </div>
+                        <div className="flex flex-row gap-[10px] justify-normal items-center border-b-[2px] pt-[15px] pb-[20px]">
+                            <div className="h-[40px] w-[40px] bg-[#CEFA05] rounded-full text-[20px] font-semibold flex justify-center items-center shrink-0">03</div>
+                            <p className="text-[13px]">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias nemo aperiam, deleniti nobis eius maxime, est reprehenderit eaque modi rerum excepturi laudantium,</p>
+                        </div>
+                        <div className="flex flex-row gap-[10px] justify-normal items-center pt-[15px]">
+                            <div className="h-[40px] w-[40px] bg-[#CEFA05] rounded-full text-[20px] font-semibold flex justify-center items-center shrink-0">04</div>
+                            <p className="text-[13px]">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias nemo aperiam, deleniti nobis eius maxime, est reprehenderit eaque modi rerum excepturi laudantium,</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            
 
 
 
