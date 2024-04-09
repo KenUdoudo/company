511a28a7f3c6919e565c87641a691dc81d9705cf
Ken Udoudo <kendo302003@gmail.com>
2024-04-09 10:25:01 -0700

    omo

---
+++ src/components/Home/section6.jsx
@@ -0,0 +1,17 @@
+const Section6 = () => {
+    return (
+        <div className="mt-[50px] px-[50px]">
+            <div className="text-center">
+                <p className="text-[28px] font-semibold">Spreading Smiles, Changing Lives</p>
+                <p className="text-[13px]">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quaerat odio maxime nobis dolorum distinctio dolor minus magni, ratione rem vero <br /> dignissimos delectus ipsa quod. Expedita animi veritatis quas laboriosam mollitia?</p>
+            </div>
+
+            <div className="border-[#dfee9b] rounded-lg border-[5px] mt-[30px]">
+                
+            </div>
+
+        </div>
+    );
+}
+ 
+export default Section6;
