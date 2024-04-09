55319ec28c1466c0f2078c802cfb9ff8a5895a9c
Ken Udoudo <kendo302003@gmail.com>
2024-04-09 10:44:09 -0700

    something wrong

--- src/components/Home/section7.jsx
+++ src/components/Home/section7.jsx
@@ -1,6 +1,15 @@
+import { FaPlus } from "react-icons/io";
+import { IoCloseSharp } from "react-icons/io5"
 const Section7 = () => {
     return (
-        <div></div>
+        <div className="mt-[50px] px-[50px]">
+            <div className="text-center">
+                <p className="text-[28px] font-semibold">Frequently Asked Questions</p>
+                <p className="text-[13px]">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quaerat odio maxime nobis dolorum distinctio dolor minus magni, ratione rem vero <br /> dignissimos delectus ipsa quod. Expedita animi veritatis quas laboriosam mollitia?</p>
+            </div>
+
+
+        </div>
     );
 }
  
