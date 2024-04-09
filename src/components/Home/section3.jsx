b1e013f2a4d156f1f26ac00ea246e0c3bd1c7dae
Ken Udoudo <kendo302003@gmail.com>
2024-04-09 08:53:16 -0700

    flex

--- src/components/Home/section3.jsx
+++ src/components/Home/section3.jsx
@@ -1,7 +1,16 @@
 const Section3 = () => {
     return (
-        <div>
-            
+        <div className="mt-[50px]">
+            <div className="text-center flex flex-col justify-center items-center">
+                <p className="text-[28px] font-semibold">Empowering Children, Creating Futures</p>
+                <p className="text-[13px]">Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime quo nesciunt est similique quidem, dignissimos pariatur ad eligendi! <br /> Aperiam harum exercitationem aut incidunt quos, perferendis vero suscipit doloribus dolorum veritatis!</p>
+                <p className="text-[13px] font-medium rounded-full px-[10px] py-[10px] bg-gray-100 mt-[20px]">Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime quo nesciunt est similique quidem, dignissimos pariatur ad eligendi! </p>
+            </div>
+
+
+
+
+
         </div>
     );
 }
