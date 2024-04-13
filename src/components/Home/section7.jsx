9dcc80491448fa4a1fed12bb80ec0104f9befdc2
Ken Udoudo <kendo302003@gmail.com>
2024-04-13 00:07:59 -0700

    fixed

--- src/components/Home/section7.jsx
+++ src/components/Home/section7.jsx
@@ -2,36 +2,55 @@ import { useState } from "react";
 import { HiPlus } from "react-icons/hi2";
 import { IoMdClose } from "react-icons/io"; 
 const Section7 = () => {
-    const [iconClicked, setIconClicked] = useState(false);
-    const [displayText2, setDisplayText2] = useState('');
-    const [displayText3, setDisplayText3] = useState('');
-    const [displayText4, setDisplayText4] = useState('');
-    const [displayText5, setDisplayText5] = useState('');
     const [displayText1, setDisplayText1] = useState('');
+    const [icon1, setIcon1] = useState(<HiPlus />);
+
+    const [displayText2, setDisplayText2] = useState('');
+    const [icon2, setIcon2] = useState(<HiPlus />);
+
+    const [displayText3, setDisplayText3] = useState('');
+    const [icon3, setIcon3] = useState(<HiPlus />);
+
+    const [displayText4, setDisplayText4] = useState('');
+    const [icon4, setIcon4] = useState(<HiPlus />);
+
+    const [displayText5, setDisplayText5] = useState('');
+    const [icon5, setIcon5] = useState(<HiPlus />);
+    
 
     const handleClick = (buttonId) => {
-        setIconClicked(prevState => !prevState);
         let message = '';
+        let icon = <HiPlus />;
         switch (buttonId) {
             case 1:
                 message = 'Text for button 1'
                 setDisplayText1(prevText => prevText === message ? '' : message);
+                icon = displayText1 === message ? <HiPlus /> : <IoMdClose />;
+                setIcon1(icon);
                 break;
             case 2:
                 message = 'Text for button 2'
                 setDisplayText2(prevText => prevText === message ? '' : message);
+                icon = displayText2 === message ? <HiPlus /> : <IoMdClose />;
+                setIcon2(icon);
                 break;
             case 3:
                 message = 'Text for button 3'
                 setDisplayText3(prevText => prevText === message ? '' : message);
+                icon = displayText3 === message ? <HiPlus /> : <IoMdClose />;
+                setIcon3(icon);
                 break;
             case 4:
                 message = 'Text for button 4'
                 setDisplayText4(prevText => prevText === message ? '' : message);
+                icon = displayText4 === message ? <HiPlus /> : <IoMdClose />;
+                setIcon4(icon);
                 break;
             case 5:
                 message = 'Text for button 5'
                 setDisplayText5(prevText => prevText === message ? '' : message);
+                icon = displayText5 === message ? <HiPlus /> : <IoMdClose />;
+                setIcon5(icon);
                 break;
             default:
                 message = '';
@@ -53,7 +72,7 @@ const Section7 = () => {
                         </div>
                     </div>
                     <div className="h-[30px] w-[30px] rounded-full bg-[#D0F49C] flex justify-center items-center" onClick={() => handleClick(1)}>
-                        {iconClicked ? <IoMdClose /> : <HiPlus />}
+                        {icon1}
                     </div>
                 </div>
                 <div className=" flex flex-row justify-between items-center px-[30px] h-[72px] bg-white rounded-md">
@@ -65,7 +84,7 @@ const Section7 = () => {
                         </div>
                     </div>
                     <div className="h-[30px] w-[30px] rounded-full bg-[#D0F49C] flex justify-center items-center" onClick={() => handleClick(2)}>
-                        {iconClicked ? <IoMdClose /> : <HiPlus />}
+                        {icon2}
                     </div>
                 </div>
                 <div className=" flex flex-row justify-between items-center px-[30px] h-[72px] bg-white rounded-md">
@@ -77,7 +96,7 @@ const Section7 = () => {
                         </div>
                     </div>
                     <div className="h-[30px] w-[30px] rounded-full bg-[#D0F49C] flex justify-center items-center" onClick={() => handleClick(3)}>
-                        {iconClicked ? <IoMdClose /> : <HiPlus />}
+                        {icon3}
                     </div>
                 </div>
                 <div className=" flex flex-row justify-between items-center px-[30px] h-[72px] bg-white rounded-md">
@@ -89,7 +108,7 @@ const Section7 = () => {
                         </div>
                     </div>
                     <div className="h-[30px] w-[30px] rounded-full bg-[#D0F49C] flex justify-center items-center" onClick={() => handleClick(4)}>
-                        {iconClicked ? <IoMdClose /> : <HiPlus />}
+                        {icon4}
                     </div>
                 </div>
                 <div className=" flex flex-row justify-between items-center px-[30px] h-[72px] bg-white rounded-md">
@@ -101,7 +120,7 @@ const Section7 = () => {
                         </div>
                     </div>
                     <div className="h-[30px] w-[30px] rounded-full bg-[#D0F49C] flex justify-center items-center" onClick={() => handleClick(5)}>
-                        {iconClicked ? <IoMdClose /> : <HiPlus />}
+                        {icon5}
                     </div>
                 </div>
             </div>
