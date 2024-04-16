e0ba6b4d82bfe2337e5d16a5dcac59eb9cf1df39
Ken Udoudo <kendo302003@gmail.com>
2024-04-16 11:03:31 -0700

    gap

--- src/components/Home/section3.jsx
+++ src/components/Home/section3.jsx
@@ -2,7 +2,7 @@ import { HiArrowUpRight } from "react-icons/hi2";
 const Section3 = () => {
     return (
         <div className="mt-[50px]">
-            <div className="text-center flex flex-col justify-center items-center">
+            <div className="text-center flex flex-col justify-center items-center gap-[10px]">
                 <p className="text-[28px] font-semibold">Empowering Children, Creating Futures</p>
                 <p className="text-[13px]">Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime quo nesciunt est similique quidem, dignissimos pariatur ad eligendi! <br /> Aperiam harum exercitationem aut incidunt quos, perferendis vero suscipit doloribus dolorum veritatis!</p>
                 <p className="text-[13px] font-medium rounded-full px-[10px] py-[10px] bg-[#E9E9E9] mt-[20px]">Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime quo nesciunt est similique quidem, dignissimos pariatur ad eligendi! </p>
