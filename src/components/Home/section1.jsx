ab74b749000da788dc4334852636d5b774fb2e42
Ken Udoudo <kendo302003@gmail.com>
2024-04-16 10:43:59 -0700

    extra css

--- src/components/Home/section1.jsx
+++ src/components/Home/section1.jsx
@@ -3,11 +3,11 @@ import { IoIosSunny } from "react-icons/io";
 import { HiSparkles } from "react-icons/hi2";
 const Section1 = () => {
     return (
-        <div className="mt-[20px]">
+        <div className="mt-[40px]">
             <div className="flex flex-col justify-center items-center text-center gap-y-[20px]">
                 <p className="text-[38px] font-[600]">Transforming Lives Through <br /> Hope and Education</p>
                 <p className="px-[80px] text-[14px]">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur repudiandae numquam autem nostrum dolor maiores sapiente eaque aliquam adipisci nam quam soluta expedita dicta, voluptate vel at nisi dolores voluptates.</p>
-                <div className='flex flex-row justify-center items-center gap-[4px] rounded-full px-[13px] py-[10px] bg-[#F7941D] cursor-pointer'>
+                <div className='flex flex-row justify-center items-center gap-[4px] rounded-full px-[13px] py-[10px] bg-[#F7941D] cursor-pointer mt-[10px]'>
                     <p className='text-[13px] font-[500]'>Donate Now</p>
                     <div className='h-[25px] w-[25px] rounded-full border-[1px] bg-[#FFD4A1] flex justify-center items-center'><HiArrowUpRight/></div>
                 </div>
