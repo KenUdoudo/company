20cf685e3f68cdb59974f6e96687f2f37a13a5f0
Ken Udoudo <kendo302003@gmail.com>
2024-06-10 11:14:04 -0700

    plenty to do

--- src/App.jsx
+++ src/App.jsx
@@ -18,13 +18,13 @@ function App() {
     setIsOpen(!isOpen);
   };
   return (
-    <div className='px-[50px] font-monts'>
-      <div className='flex flex-row justify-between items-center bg-gray-100 rounded-full px-[15px] py-[15px] mt-[10px] w-full'>
+    <div className='px-[20px] lg:px-[50px] font-monts'>
+      <div className='flex flex-row justify-between items-center bg-white md:bg-gray-100 rounded-full px-[15px] py-[15px] mt-[10px] w-full'>
         <div className=" w-[100px]"><img src={ logo }/></div>
 
         <button onClick={toggleMenu} className='block md:hidden z-30'><HiOutlineMenu className="h-[25px] w-[25px]"/></button>
 
-        <div className={`fixed top-0 right-0 h-full bg-[#e9e9e9] w-[75%] transform ${isOpen ? '-translate-x-0' : 'translate-x-full'} transition-transform duration-500 ease-in-out overflow-hidden`}>
+       {/* <div className={`fixed top-0 right-0 h-full bg-[#e9e9e9] w-[75%] transform ${isOpen ? '-translate-x-0' : 'translate-x-full'} transition-transform duration-500 ease-in-out overflow-hidden`}>
           <div className="flex flex-col items-start gap-y-[20px] px-[20px] py-[20px]">
             <button className={`${tab === 0 ? " bg-[#E9E9E9]" : ""} rounded-full px-[15px] py-[3px] text-[16px] font-[500]`} onClick={()=> setTab(0)}>Home</button>
             <button className={`${tab === 1 ? " bg-[#E9E9E9]" : ""} rounded-full px-[15px] py-[3px] text-[16px] font-[500]`} onClick={()=> setTab(1)}>Get involved</button>
@@ -37,7 +37,7 @@ function App() {
             <p className='text-[13px] font-[500]'>Contact Us</p>
             <div className='h-[25px] w-[25px] rounded-full border-[1px] bg-[#FFD4A1] flex justify-center items-center'><HiMiniEnvelope/></div>
           </div>
-        </div>
+        </div>*/}
 
         <div className="hidden md:flex flex-row justify-center items-center gap-x-[15px]">
           <button className={`${tab === 0 ? " bg-[#E9E9E9]" : ""} rounded-full px-[15px] py-[3px] text-[16px] font-[500]`} onClick={()=> setTab(0)}>Home</button>
