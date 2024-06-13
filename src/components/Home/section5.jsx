29ad8a55349b8eb18263247bf85bc664c19e349a
Ken Udoudo <kendo302003@gmail.com>
2024-06-13 11:33:16 -0700

    almost there

--- src/components/Home/section5.jsx
+++ src/components/Home/section5.jsx
@@ -1,7 +1,7 @@
 import { HiArrowUpRight } from "react-icons/hi2";
 const Section5 = () => {
     return (
-        <div className="px-[50px] mt-[70px]">
+        <div className="px-[20px] lg:px-[50px] mt-[70px]">
             <div className="flex flex-col justify-center items-center">
                 <div className="bg-[#FFD4A1] rounded-lg border-[5px] h-[300px] w-full"></div>
                 <div className="flex flex-col justify-center items-center text-center w-[70%] bg-white mt-[-100px] p-[30px] rounded-lg">
@@ -10,22 +10,22 @@ const Section5 = () => {
                 </div>
             </div>
 
-            <div className="flex flex-row justify-center items-center bg-[#E9E9E9] p-[30px] rounded-lg mt-[30px] space-x-[30px]">
-                <div className="flex flex-col justify-start w-[33%] bg-white rounded-lg p-[20px] gap-[7px]">
+            <div className="flex flex-col md:flex-row justify-center bg-[#E9E9E9] p-[30px] rounded-lg mt-[30px] gap-x-[30px] gap-y-[20px]">
+                <div className="flex flex-col justify-start w-full md:w-[33%] bg-white rounded-lg p-[20px] gap-[7px]">
                     <div className="flex flex-row justify-between items-center ">
                         <p className="font-semibold">Needs Assessment</p>
                         <HiArrowUpRight />
                     </div>
                     <p className="text-[13px]">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam quae maxime qui. Minima adipisci id, amet distinctio nihil nostrum, quae illum nobis,</p>
                 </div>
-                <div className="flex flex-col justify-start w-[33%] bg-white rounded-lg p-[20px] gap-[7px]">
+                <div className="flex flex-col justify-start w-full md:w-[33%] bg-white rounded-lg p-[20px] gap-[7px]">
                     <div className="flex flex-row justify-between items-center ">
                         <p className="font-semibold">Collaborative Partnerships</p>
                         <HiArrowUpRight />
                     </div>
                     <p className="text-[13px]">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam quae maxime qui. Minima adipisci id, amet distinctio nihil nostrum, quae illum nobis,</p>
                 </div>
-                <div className="flex flex-col justify-start w-[33%] bg-white rounded-lg p-[20px] gap-[7px]">
+                <div className="flex flex-col justify-start w-full md:w-[33%] bg-white rounded-lg p-[20px] gap-[7px]">
                     <div className="flex flex-row justify-between items-center ">
                         <p className="font-semibold">Transparent Impact Reporting</p>
                         <HiArrowUpRight />
