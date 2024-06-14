f7e5db6b4de3e9acf0ab50cae8cd11d41edf1b29
Ken Udoudo <kendo302003@gmail.com>
2024-06-14 14:56:15 -0700

    text update

--- src/components/Home/section3.jsx
+++ src/components/Home/section3.jsx
@@ -3,16 +3,16 @@ const Section3 = () => {
     return (
         <div className="mt-[50px]">
             <div className="text-center flex flex-col justify-center items-center gap-[10px]">
-                <p className="text-[28px] font-semibold">Empowering Children, Creating Futures</p>
-                <p className="text-[13px]">Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime quo nesciunt est similique quidem, dignissimos pariatur ad eligendi! <br /> Aperiam harum exercitationem aut incidunt quos, perferendis vero suscipit doloribus dolorum veritatis!</p>
-                <p className="text-[13px] font-medium rounded-lg md:rounded-full px-[10px] py-[10px] bg-[#E9E9E9] mt-[20px]">Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime quo nesciunt est similique quidem, dignissimos pariatur ad eligendi! </p>
+                <p className="text-[28px] font-semibold">Turning Small wins, into BIG Ones!</p>
+                <p className="text-[13px]">We are recognizing & celebrating academic & vocational achievements at a small level, to inspire the next BIG thing. </p>
+                <p className="text-[13px] font-medium rounded-lg md:rounded-full px-[10px] py-[10px] bg-[#E9E9E9] mt-[20px]">Our foundation believes in the depth of human potential. We can unlock at with just enough encouragement.</p>
             </div>
 
             <div className="flex flex-row justify-center items-center px-[20px] lg:px-[50px] gap-x-[30px] mt-[20px] md:h-[660px] lg:h-[520px]">
                 <div className="hidden md:block md:w-[45%] lg:w-[40%] bg-[#FFD4A1] rounded-lg border-[5px]  h-full"></div>
                 <div className="w-full md:w-[60%] bg-[#E9E9E9] p-[30px] rounded-lg flex flex-col justify-center gap-y-[30px] h-full">
                     <div className="flex flex-row justify-between items-center">
-                        <p className="font-semibold text-[20px]">Mission</p>
+                        <p className="font-semibold text-[20px]">Goals</p>
                         <div className='flex flex-row justify-center items-center gap-[4px] rounded-full bg-white px-[13px] py-[10px] cursor-pointer'>
                             <p className='text-[13px] font-roboto font-[500]'>Learn More</p>
                             <HiArrowUpRight/>
@@ -22,19 +22,19 @@ const Section3 = () => {
                     <div className="flex flex-col justify-center items-center bg-white rounded-lg p-[20px]">
                         <div className="flex flex-row gap-[10px] justify-normal items-center border-b-[2px] pb-[20px]">
                             <div className="h-[40px] w-[40px] bg-[#F7941D] rounded-full text-[20px] font-semibold flex justify-center items-center shrink-0">01</div>
-                            <p className="text-[13px]">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias nemo aperiam, deleniti nobis eius maxime, est reprehenderit eaque modi rerum excepturi laudantium,</p>
+                            <p className="text-[13px]">Enhance Education at access and quality.</p>
                         </div>
                         <div className="flex flex-row gap-[10px] justify-normal items-center border-b-[2px] pt-[15px] pb-[20px]">
                             <div className="h-[40px] w-[40px] bg-[#F7941D] rounded-full text-[20px] font-semibold flex justify-center items-center shrink-0">02</div>
-                            <p className="text-[13px]">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias nemo aperiam, deleniti nobis eius maxime, est reprehenderit eaque modi rerum excepturi laudantium,</p>
+                            <p className="text-[13px]">More vocational shill development & entrepreneurship.</p>
                         </div>
                         <div className="flex flex-row gap-[10px] justify-normal items-center border-b-[2px] pt-[15px] pb-[20px]">
                             <div className="h-[40px] w-[40px] bg-[#F7941D] rounded-full text-[20px] font-semibold flex justify-center items-center shrink-0">03</div>
-                            <p className="text-[13px]">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias nemo aperiam, deleniti nobis eius maxime, est reprehenderit eaque modi rerum excepturi laudantium,</p>
+                            <p className="text-[13px]">Foster a culture of academic a vocational excellence.</p>
                         </div>
                         <div className="flex flex-row gap-[10px] justify-normal items-center pt-[15px]">
                             <div className="h-[40px] w-[40px] bg-[#F7941D] rounded-full text-[20px] font-semibold flex justify-center items-center shrink-0">04</div>
-                            <p className="text-[13px]">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias nemo aperiam, deleniti nobis eius maxime, est reprehenderit eaque modi rerum excepturi laudantium,</p>
+                            <p className="text-[13px]">Build strong partnership with communities, schools 2 businesses to support youth development initiatives.</p>
                         </div>
                     </div>
                 </div>
@@ -43,7 +43,7 @@ const Section3 = () => {
                 <div className="hidden md:block md:w-[45%] lg:w-[40%] bg-[#FFD4A1] rounded-lg border-[5px] h-full"></div>
                 <div className="w-full md:w-[60%] bg-[#E9E9E9] p-[30px] rounded-lg flex flex-col justify-center space-y-[30px] h-full">
                     <div className="flex flex-row justify-between items-center">
-                        <p className="font-semibold text-[20px]">Vision</p>
+                        <p className="font-semibold text-[20px]">Objectives</p>
                         <div className='flex flex-row justify-center items-center gap-[4px] rounded-full bg-white px-[13px] py-[10px] cursor-pointer'>
                             <p className='text-[13px] font-roboto font-[500]'>Learn More</p>
                             <HiArrowUpRight/>
@@ -53,19 +53,19 @@ const Section3 = () => {
                     <div className="flex flex-col justify-center items-center bg-white rounded-lg p-[20px]">
                         <div className="flex flex-row gap-[10px] justify-normal items-center border-b-[2px] pb-[20px]">
                             <div className="h-[40px] w-[40px] bg-[#F7941D] rounded-full text-[20px] font-semibold flex justify-center items-center shrink-0">01</div>
-                            <p className="text-[13px]">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias nemo aperiam, deleniti nobis eius maxime, est reprehenderit eaque modi rerum excepturi laudantium,</p>
+                            <p className="text-[13px]">Provide scholarships 2 financial aid to at least 100 students annually.</p>
                         </div>
                         <div className="flex flex-row gap-[10px] justify-normal items-center border-b-[2px] pt-[15px] pb-[20px]">
                             <div className="h-[40px] w-[40px] bg-[#F7941D] rounded-full text-[20px] font-semibold flex justify-center items-center shrink-0">02</div>
-                            <p className="text-[13px]">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias nemo aperiam, deleniti nobis eius maxime, est reprehenderit eaque modi rerum excepturi laudantium,</p>
+                            <p className="text-[13px]">Partner with local businesses to secure Intersi ship 2 apprenticeship opportunities for at least 50 train each year.</p>
                         </div>
                         <div className="flex flex-row gap-[10px] justify-normal items-center border-b-[2px] pt-[15px] pb-[20px]">
                             <div className="h-[40px] w-[40px] bg-[#F7941D] rounded-full text-[20px] font-semibold flex justify-center items-center shrink-0">03</div>
-                            <p className="text-[13px]">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias nemo aperiam, deleniti nobis eius maxime, est reprehenderit eaque modi rerum excepturi laudantium,</p>
+                            <p className="text-[13px]">Establish annual awards ceremony to recognize top-performing students and trainees in both academic & Vocational fields.</p>
                         </div>
                         <div className="flex flex-row gap-[10px] justify-normal items-center pt-[15px]">
                             <div className="h-[40px] w-[40px] bg-[#F7941D] rounded-full text-[20px] font-semibold flex justify-center items-center shrink-0">04</div>
-                            <p className="text-[13px]">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias nemo aperiam, deleniti nobis eius maxime, est reprehenderit eaque modi rerum excepturi laudantium,</p>
+                            <p className="text-[13px]">Introduce life shills 2L leadership training modules into all educational & Vocational programs.</p>
                         </div>
                     </div>
                 </div>
--- src/components/Home/section4.jsx
+++ src/components/Home/section4.jsx
@@ -5,25 +5,25 @@ const Section4 = () => {
             <div className="flex flex-col justify-center items-center">
                 <div className="bg-[#FFD4A1] rounded-lg border-[5px] h-[200px] md:h-[300px] w-full"></div>
                 <div className="flex flex-col justify-center items-center text-center w-[90%] md:w-[70%] bg-white mt-[-100px] p-[30px] rounded-lg">
-                    <p className="text-[20px] md:text-[28px] font-semibold">Goals and Objectives</p>
-                    <p className="text-[13px]">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Similique fugiat modi ducimus in perspiciatis iusto voluptas, magnam omnis culpa deleniti ex, molestiae repudiandae ullam laboriosam sapiente nemo voluptate et natus?</p>
+                    <p className="text-[20px] md:text-[28px] font-semibold">Our Vision</p>
+                    <p className="text-[13px]">Empowering Nigerian youth to achieve academic and vocational excellence, fostering a culture of innovation, entrepreneurship a lifelong hearing.</p>
                 </div>
             </div>
 
             <div className="flex flex-col md:flex-row justify-center bg-[#E9E9E9] p-[30px] rounded-lg mt-[30px] gap-x-[30px] gap-y-[20px]">
                 <div className="flex flex-col justify-start w-full md:w-[50%] bg-white rounded-lg p-[20px] gap-[7px]">
                     <div className="flex flex-row justify-between items-center ">
-                        <p className="font-semibold">The goal of ForHelp</p>
+                        <p className="font-semibold">Empowering a self-reliant nation</p>
                         <HiArrowUpRight />
                     </div>
-                    <p className="text-[13px]">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam quae maxime qui. Minima adipisci id, amet distinctio nihil nostrum, quae illum nobis,</p>
+                    <p className="text-[13px]">We see a future where every young Nigerian is empowered to dream BIG and innovate towards a self-reliant nation.</p>
                 </div>
                 <div className="flex flex-col justify-start w-full md:w-[50%] bg-white rounded-lg p-[20px] gap-[7px]">
                     <div className="flex flex-row justify-between items-center ">
-                        <p className="font-semibold">The organization's objective</p>
+                        <p className="font-semibold">Catalyzing global readers</p>
                         <HiArrowUpRight />
                     </div>
-                    <p className="text-[13px]">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam quae maxime qui. Minima adipisci id, amet distinctio nihil nostrum, quae illum nobis,</p>
+                    <p className="text-[13px]">We aim to be the catalyst for transforming Nigeria's youth into globally competitive leaders & skilled professionals.</p>
                 </div>
 
 
