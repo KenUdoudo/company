66f941519de131abd105899681e3af16f1aeef7e
Ken Udoudo <kendo302003@gmail.com>
2024-06-14 14:56:15 -0700

    text update

--- src/App.jsx
+++ src/App.jsx
@@ -30,11 +30,11 @@ function App() {
         <div className={`fixed top-0 right-0 h-full bg-[#e9e9e9] w-[75%] transform ${isOpen ? '-translate-x-0' : 'translate-x-full'} transition-transform duration-500 ease-in-out overflow-hidden`}>
             <div className="flex flex-col items-start gap-y-[20px] px-[20px] py-[20px]">
               <Link to="/" className=" hover:bg-[#E9E9E9] rounded-full px-[15px] py-[3px] text-[16px] font-[500]" >Home</Link>
-              <Link to="/getinvolved" className=" hover:bg-[#E9E9E9] rounded-full px-[15px] py-[3px] text-[16px] font-[500]" >Get Involved</Link>
-              <Link to="/donate" className=" hover:bg-[#E9E9E9] rounded-full px-[15px] py-[3px] text-[16px] font-[500]" >Donate</Link>
-              <Link to="/impactareas" className=" hover:bg-[#E9E9E9] rounded-full px-[15px] py-[3px] text-[16px] font-[500]" >Impact Areas</Link>
-              <Link to="/successstories" className=" hover:bg-[#E9E9E9] rounded-full px-[15px] py-[3px] text-[16px] font-[500]" >Success Stories</Link>
-              <Link to="/news" className=" hover:bg-[#E9E9E9] rounded-full px-[15px] py-[3px] text-[16px] font-[500]" >News</Link>
+              <Link to="/gallery" className=" hover:bg-[#E9E9E9] rounded-full px-[15px] py-[3px] text-[16px] font-[500]" >Gallery</Link>
+              <Link to="/contribute" className=" hover:bg-[#E9E9E9] rounded-full px-[15px] py-[3px] text-[16px] font-[500]" >Contribute</Link>
+              <Link to="/stories" className=" hover:bg-[#E9E9E9] rounded-full px-[15px] py-[3px] text-[16px] font-[500]" >Stories</Link>
+              <Link to="/blog" className=" hover:bg-[#E9E9E9] rounded-full px-[15px] py-[3px] text-[16px] font-[500]" >Blog</Link>
+              <Link to="/aboutus" className=" hover:bg-[#E9E9E9] rounded-full px-[15px] py-[3px] text-[16px] font-[500]" >About Us</Link>
             </div>
             <div className='flex flex-row justify-center items-center gap-[4px] px-[13px] py-[10px] bg-[#F7941D] cursor-pointer'>
               <p className='text-[13px] font-[500]'>Contact Us</p>
@@ -44,11 +44,11 @@ function App() {
 
           <div className="hidden md:flex flex-row justify-center items-center gap-x-[4px] lg:gap-x-[15px]">
             <Link to="/" className="hover:bg-[#E9E9E9] rounded-full px-[10px] lg:px-[15px] py-[3px] text-nowrap text-[13px] lg:text-[16px] font-[500]">Home</Link>
-            <Link to="/getinvolved" className="hover:bg-[#E9E9E9] rounded-full px-[10px] lg:px-[15px] py-[3px] text-nowrap text-[13px] lg:text-[16px] font-[500]">Get Involved</Link>
-            <Link to="/donate" className="hover:bg-[#E9E9E9] rounded-full px-[10px] lg:px-[15px] py-[3px] text-nowrap text-[13px] lg:text-[16px] font-[500]">Donate</Link>
-            <Link to="/impactareas" className="hover:bg-[#E9E9E9] rounded-full px-[10px] lg:px-[15px] py-[3px] text-nowrap text-[13px] lg:text-[16px] font-[500]">Impact Areas</Link>
-            <Link to="/successstories" className="hover:bg-[#E9E9E9] rounded-full px-[10px] lg:px-[15px] py-[3px] text-nowrap text-[13px] lg:text-[16px] font-[500]">Success Stories</Link>
-            <Link to="/news" className="hover:bg-[#E9E9E9] rounded-full px-[10px] lg:px-[15px] py-[3px] text-nowrap text-[13px] lg:text-[16px] font-[500]">News</Link>
+            <Link to="/gallery" className="hover:bg-[#E9E9E9] rounded-full px-[10px] lg:px-[15px] py-[3px] text-nowrap text-[13px] lg:text-[16px] font-[500]">Gallery</Link>
+            <Link to="/contribute" className="hover:bg-[#E9E9E9] rounded-full px-[10px] lg:px-[15px] py-[3px] text-nowrap text-[13px] lg:text-[16px] font-[500]">Contribute</Link>
+            <Link to="/stories" className="hover:bg-[#E9E9E9] rounded-full px-[10px] lg:px-[15px] py-[3px] text-nowrap text-[13px] lg:text-[16px] font-[500]">Stories</Link>
+            <Link to="/blog" className="hover:bg-[#E9E9E9] rounded-full px-[10px] lg:px-[15px] py-[3px] text-nowrap text-[13px] lg:text-[16px] font-[500]">Blog</Link>
+            <Link to="/aboutus" className="hover:bg-[#E9E9E9] rounded-full px-[10px] lg:px-[15px] py-[3px] text-nowrap text-[13px] lg:text-[16px] font-[500]">About Us</Link>
           </div>
           <div className='hidden md:flex flex-row justify-center items-center gap-[4px] rounded-full px-[13px] py-[10px] bg-[#F7941D] cursor-pointer'>
             <p className='text-[13px] font-[500]'>Contact Us</p>
@@ -59,23 +59,23 @@ function App() {
         <div className='content'>
           <Routes>
             <Route path='/' element={<Home />} />
-            <Route path='/getinvolved' element={<GetInvolved />} />
-            <Route path='/donate' element={<Donate />} />
-            <Route path='/impactareas' element={<ImpactAreas />} />
-            <Route path='/successstories' element={<SuccessStories />} />
-            <Route path='/news' element={<News />} />
+            <Route path='/gallery' element={<GetInvolved />} />
+            <Route path='/contribute' element={<Donate />} />
+            <Route path='/stories' element={<ImpactAreas />} />
+            <Route path='/blog' element={<SuccessStories />} />
+            <Route path='/aboutus' element={<News />} />
           </Routes>
         </div>
 
         <div className='hidden md:flex flex-row justify-between items-center bg-gray-100 rounded-full px-[20px] py-[20px] mt-[80px]'>
         <div className=" w-[100px]"><img src={ logo }/></div>
           <div className="flex flex-row justify-center items-center gap-x-[4px] lg:gap-x-[15px]">
-            <Link to="/" className="rounded-full px-[15px] py-[3px] text-nowrap text-[13px] font-[500]" onClick={()=> setTab(0)}>Home</Link>
-            <Link to="/getinvolved" className="rounded-full px-[15px] py-[3px] text-nowrap text-[13px] font-[500]" onClick={()=> setTab(1)}>Get involved</Link>
-            <Link to="/donate" className="rounded-full px-[15px] py-[3px] text-nowrap text-[13px] font-[500]" onClick={()=> setTab(2)}>Donate</Link>
-            <Link to="/impactareas" className="rounded-full px-[15px] py-[3px] text-nowrap text-[13px] font-[500]" onClick={()=> setTab(3)}>Impact Areas</Link>
-            <Link to="/successstories" className="rounded-full px-[15px] py-[3px] text-nowrap text-[13px] font-[500]" onClick={()=> setTab(4)}>Success Stories</Link>
-            <Link to="/news" className="rounded-full px-[15px] py-[3px] text-nowrap text-[13px] font-[500]" onClick={()=> setTab(5)}>News</Link>
+            <Link to="/" className="rounded-full px-[15px] py-[3px] text-nowrap text-[13px] font-[500]">Home</Link>
+            <Link to="/gallery" className="rounded-full px-[15px] py-[3px] text-nowrap text-[13px] font-[500]">Gallery</Link>
+            <Link to="/contribute" className="rounded-full px-[15px] py-[3px] text-nowrap text-[13px] font-[500]">Contribute</Link>
+            <Link to="/stories" className="rounded-full px-[15px] py-[3px] text-nowrap text-[13px] font-[500]">Stories</Link>
+            <Link to="/blog" className="rounded-full px-[15px] py-[3px] text-nowrap text-[13px] font-[500]">Blog</Link>
+            <Link to="/aboutus" className="rounded-full px-[15px] py-[3px] text-nowrap text-[13px] font-[500]">About Us</Link>
           </div>
           <div className='flex flex-row justify-center items-center space-x-[10px]'>
             <div className="h-[30px] w-[30px] rounded-full bg-[#F7941D] flex justify-center items-center"><IoLogoFacebook/></div>
--- src/components/Home/section2.jsx
+++ src/components/Home/section2.jsx
@@ -2,22 +2,22 @@ const Section2 = () => {
     return (
         <div className=" mt-[50px]">
             <div className="text-center">
-                <p className="text-[28px] font-semibold">Give a Child Hope Today</p>
-                <p className="text-[13px]">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed, commodi architecto laborum illum incidunt voluptatem. </p>
+                <p className="text-[28px] font-semibold">Building The Nigerian capacity</p>
+                <p className="text-[13px]">Our human resources exceeds our natural resources and they lay dormant awaiting exploration.</p>
             </div>
 
             <div className="flex flex-row justify-center px-[20px] lg:px-[50px] mt-[30px]">
                 <div className="border-[2px] rounded-l-[15px] flex flex-col justify-center items-center text-center bg-gray-100 gap-[5px] px-[20px] py-[20px]">
-                    <p className="text-[20px] md:text-[28px] font-semibold bg-[#E9E9E9] px-[15px] rounded-md">85%</p>
-                    <p className="text-[8px] md:text-[13px]">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestiae quis numquam dignissimos aliquam debitis cum magnam suscipit!</p>
+                    <p className="text-[20px] md:text-[28px] font-semibold bg-[#E9E9E9] px-[15px] rounded-md">67%</p>
+                    <p className="text-[8px] md:text-[13px]">of entrepreneurs are youths</p>
                 </div>
                 <div className="border-[2px] flex flex-col justify-center items-center text-center bg-gray-100 gap-[5px] px-[20px] py-[20px]">
-                    <p className="text-[20px] md:text-[28px] font-semibold bg-[#E9E9E9] px-[15px] rounded-md">3,250</p>
-                    <p className="text-[8px] md:text-[13px]">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestiae quis numquam dignissimos aliquam debitis cum magnam suscipit!</p>
+                    <p className="text-[20px] md:text-[28px] font-semibold bg-[#E9E9E9] px-[15px] rounded-md">80%</p>
+                    <p className="text-[8px] md:text-[13px]">Of Nigerian Youths are tech-Savvy</p>
                 </div>
                 <div className="border-[2px] rounded-r-[15px] flex flex-col justify-center items-center text-center bg-gray-100 gap-[5px] px-[20px] py-[20px]">
-                    <p className="text-[20px] md:text-[28px] font-semibold bg-[#E9E9E9] px-[15px] rounded-md">69%</p>
-                    <p className="text-[8px] md:text-[13px]">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestiae quis numquam dignissimos aliquam debitis cum magnam suscipit!</p>
+                    <p className="text-[20px] md:text-[28px] font-semibold bg-[#E9E9E9] px-[15px] rounded-md">68%</p>
+                    <p className="text-[8px] md:text-[13px]">completed secondary education.</p>
                 </div>
             </div>
         </div>
