5f29de256f6fa5481cdebf5db5030324d968a994
Ken Udoudo <kendo302003@gmail.com>
2024-06-15 00:44:13 -0700

    done with text

--- src/components/Home/accordion.jsx
+++ src/components/Home/accordion.jsx
@@ -30,18 +30,53 @@ const Acc = () => {
     const items = [
         {
             number: '01',
-            title: 'Accordion Item 1',
-            content: 'This is the content for item 1.'
+            title: 'How can I help PLEF?',
+            content: (
+                   <div>
+                        <p>There are several ways to support our project:</p>
+                        <ul className="list-disc pl-5">
+                            <li>Volunteer →  Join us as a volunteer to assist in various programs and activities.</li>
+                            <li>Donate → Financial contributions are always welcome to help us expand our reach and impact.</li>
+                            <li>Partner → Collaborate with us as a corporate or community partner to provide resources, expertise, or opportunities for our beneficiaries.</li>
+                            <li>Advocate → Spread the word about our mission and programs to raise awareness and garner more support.</li>
+                        </ul>
+                   </div> 
+            ),
         },
         {
             number: '02',
-            title: 'Accordion Item 2',
-            content: 'This is the content for item 2.'
+            title: 'How does PLEF receive donations and other contributions?',
+            content: 'Donations can be made through our website via secure online payment methods. We also accept contributions via bank transfers, checks, and in-kind donations. For corporate sponsorships and larger contributions, please contact our fundraising team directly.'
         },
         {
             number: '03',
-            title: 'Accordion Item 3',
-            content: 'This is the content for item 3.'
+            title: 'How can I benefit from PLEF?',
+            content: (
+                <div>
+                     <p>If you are a young person in Nigeria, you can benefit from our project by:</p>
+                     <ul className="list-disc pl-5">
+                         <li>Scholarships: Access financial aid to support your education.</li>
+                         <li>Participating in Training Programs: Enroll in our vocational training courses to gain marketable skills.</li>
+                         <li>Receiving Mentorship: Benefit from mentorship opportunities with experienced professionals.</li>
+                         <li>Joining Workshops and Events: Attend our workshops and events to enhance your personal and professional development.</li>
+                     </ul>
+                </div> 
+         ),
+        },
+        {
+            number: '04',
+            title: 'Who is eligible to benefit from PLEF?',
+            content: 'Our programs are designed for young people in Nigeria, typically aged between 15 and 30 years. We prioritize individuals from underserved communities, those with financial needs, and those who show a strong commitment to academic and vocational excellence.'
+        },
+        {
+            number: '05',
+            title: 'What types of vocational training programs are offered?',
+            content: 'While we do not offer training directly we partner with local skilled men to provide these vocational training programs in trades such as carpentry, tailoring, ICT, plumbing, agriculture, and more. Each program is designed to provide practical, hands-on experience and is led by experienced instructors.'
+        },
+        {
+            number: '06',
+            title: 'How does PLEF select beneficiaries?',
+            content: 'Participants are selected based on a combination of factors, including, academic performance, financial need, commitment to personal and professional development, and recommendations from community leaders and educators. An application process is in place to ensure fairness and transparency.'
         }
     ];
     return (
