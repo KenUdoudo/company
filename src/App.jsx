bc7669600253fdfdef854dfa7eb9de2d1352567d
Ken Udoudo <kendo302003@gmail.com>
2024-08-22 10:33:58 +0100

    mailto

--- src/App.jsx
+++ src/App.jsx
@@ -115,21 +115,21 @@ function App() {
                 About Us
               </Link>
             </div>
-            <div className="flex flex-row justify-center items-center gap-[4px] px-[13px] py-[10px] bg-[#F7941D] cursor-pointer">
+            <a href="mailto:officialplefoundation@gmail.com"><div className="flex flex-row justify-center items-center gap-[4px] px-[13px] py-[10px] bg-[#F7941D] cursor-pointer">
               <p className="text-[13px] font-[500]">Contact Us</p>
               <div className="h-[25px] w-[25px] rounded-full border-[1px] bg-[#FFD4A1] flex justify-center items-center">
                 <HiMiniEnvelope />
               </div>
-            </div>
+            </div></a>
           </div>
 
           
-          <div className="hidden md:flex flex-row justify-center items-center gap-[4px] rounded-full px-[13px] py-[10px] bg-[#F7941D] cursor-pointer">
+          <a href="mailto:officialplefoundation@gmail.com"><div className="hidden md:flex flex-row justify-center items-center gap-[4px] rounded-full px-[13px] py-[10px] bg-[#F7941D] cursor-pointer">
             <p className="text-[13px] font-[500]">Contact Us</p>
             <div className="h-[25px] w-[25px] rounded-full border-[1px] bg-[#FFD4A1] flex justify-center items-center">
               <HiMiniEnvelope />
             </div>
-          </div>
+          </div></a>
         </div>
 
         <div className="content">
