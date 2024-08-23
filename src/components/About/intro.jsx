ba353b86f5bfdf46db899f4ff763cd88f1d94826
Ken Udoudo <kendo302003@gmail.com>
2024-08-23 18:35:47 +0100

    font

--- src/components/About/intro.jsx
+++ src/components/About/intro.jsx
@@ -1,6 +1,12 @@
 const Intro = () => {
     return (
-        <div className="flex justify-between items-center"></div>
+        <div className="flex justify-between items-center">
+            <div className="flex flex-col items-start gap-[25px]">
+                <p className="text-[40px]">Hey! <br /><span className="font-[700]">We&apos;re PLEFoundation 😊</span> </p>
+                <p className="text-[20px]">We&apos;re an optimistic and gratitude-filled group of remote workers scattered around the world and dedicated to creating a product our customers will use and love.</p>
+                <p className="text-[20px]">Read on to learn more about Buffer’s story and history and see the full Buffer team.</p>
+            </div>
+        </div>
     );
 }
  
