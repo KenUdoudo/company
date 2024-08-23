843c1a7c9e8a87d47969db5bc8ddf14136e66438
Ken Udoudo <kendo302003@gmail.com>
2024-08-23 18:40:58 +0100

    top bit

--- src/components/About/intro.jsx
+++ src/components/About/intro.jsx
@@ -1,11 +1,14 @@
+import intro from './website.svg'
+
 const Intro = () => {
     return (
-        <div className="flex justify-between items-center">
+        <div className="flex justify-between items-center gap-[20px] mt-[40px]">
             <div className="flex flex-col items-start gap-[25px]">
                 <p className="text-[40px]">Hey! <br /><span className="font-[700]">We&apos;re PLEFoundation 😊</span> </p>
                 <p className="text-[20px]">We&apos;re an optimistic and gratitude-filled group of remote workers scattered around the world and dedicated to creating a product our customers will use and love.</p>
                 <p className="text-[20px]">Read on to learn more about Buffer’s story and history and see the full Buffer team.</p>
             </div>
+            <img src={ intro }/>
         </div>
     );
 }
