b8d6bbd3ed1bf6b8ca8c711c8d151417f2727b70
Ken Udoudo <kendo302003@gmail.com>
2024-08-24 21:10:03 +0100

    working

--- src/components/About/ourstory.jsx
+++ src/components/About/ourstory.jsx
@@ -1,7 +1,16 @@
+import application from './application.svg';
+import apps from './apps.svg';
+import heart from './heart.svg';
+import likepost from './likepost.svg';
 const Story = () => {
     return (
-        <div>
-            
+        <div className="flex justify-between items-start gap-[20px] mt-[20px]">
+            <div className="w-[50%] relative">
+                <img src={ application } className='w-[25%] bg-[#FFD4A1] ml-[30px] p-[20px] rounded-md'/>
+                <img src={ apps } className='w-[40%] bg-[#FFD4A1] mt-[30px] p-[20px] rounded-md'/>
+                <img src={ likepost } className='w-[50%] bg-[#FFD4A1] absolute right-[5%] top-[25%] p-[20px] rounded-md'/>
+                <img src={ heart } className='w-[20%] bg-[#FFD4A1] absolute right-[30%] bottom-[-30%] p-[20px] rounded-md'/>
+            </div>
         </div>
     );
 }
