c9aabef41dd9bece5fb5f446c295ffc8c2cf6c8d
Ken Udoudo <kendo302003@gmail.com>
2024-08-25 13:49:51 +0100

    p

--- src/components/About/metrics.jsx
+++ src/components/About/metrics.jsx
@@ -1,7 +1,8 @@
 const Metrics = () => {
     return (
         <div>
-            <p className="text-[30px] md:text-[40px] font-[700]">We are an <span className="italic font-[400] ">Open</span> Company</p>
+            <p className="text-[30px] md:text-[40px] font-[700] text-center">We are an <span className="italic font-[400] ">Open</span> Company</p>
+            <p className="text-center">Since 2013, we&apos;ve been open with Buffer&apos;s finances and our team&apos;s salaries, among many other metrics. We share openly because we believe in the power of transparency to build trust, hold us accountable to a high standard, and push our industry forward.</p>
         </div>
     );
 }
