ddc36651600843c7a11613df51c785e0c3e2fc7b
Ken Udoudo <kendo302003@gmail.com>
2024-08-25 13:46:01 +0100

    metrics

---
+++ src/components/About/metrics.jsx
@@ -0,0 +1,9 @@
+const Metrics = () => {
+    return (
+        <div>
+            <p className="text-[30px] md:text-[40px] font-[700]">We are an <span className="italic font-[400] ">Open</span> Company</p>
+        </div>
+    );
+}
+ 
+export default Metrics;
