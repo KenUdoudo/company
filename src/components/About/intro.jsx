eaca22d7c9a22567206639f0fb73738535487dd5
Ken Udoudo <kendo302003@gmail.com>
2024-09-02 18:03:27 +0100

    text update

--- src/components/About/intro.jsx
+++ src/components/About/intro.jsx
@@ -4,9 +4,9 @@ const Intro = () => {
     return (
         <div className="flex flex-col md:flex-row justify-between items-center gap-[20px] mt-[40px]">
             <div className="flex flex-col items-start gap-[25px]">
-                <p className="text-[30px] md:text-[40px] text-center md:text-left">Hey! <br /><span className="font-[700]">We&apos;re PLEFoundation 😊</span> </p>
-                <p className="text-[20px] text-center md:text-left">We&apos;re an optimistic and gratitude-filled group of remote workers scattered around the world and dedicated to creating a product our customers will use and love.</p>
-                <p className="text-[20px] text-center md:text-left">Read on to learn more about Buffer’s story and history and see the full Buffer team.</p>
+                <p className="text-[30px] md:text-[40px] text-center md:text-left">Hey! <br /><span className="font-[700]">Welcome to PLEF! 😊</span> </p>
+                <p className="text-[20px] text-center md:text-left">What&apos;s PLEF? It&apos;s short for Piuonoshaba Life Empowerment Foundation. </p>
+                <p className="text-[20px] text-center md:text-left">The foundation is committed to empowering Nigerian youths through life to make them truly become the leaders of tomorrow! </p>
             </div>
             <img src={ intro } className='w-full md:w-[40%] lg:w-[50%]'/>
         </div>
