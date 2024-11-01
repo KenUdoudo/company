7e272f2d8f907bd2d7fb86647c10d61f708bb515
Ken Udoudo <kendo302003@gmail.com>
2024-11-01 20:54:26 +0100

    saved

--- src/components/About/intro.jsx
+++ src/components/About/intro.jsx
@@ -3,7 +3,7 @@ import intro from './intro.png';
 const Intro = () => {
     return (
         <div className="flex flex-col md:flex-row justify-between items-center gap-[20px] mt-[40px]">
-            <div className="flex flex-col items-start gap-[25px]">
+            <div className="flex flex-col gap-[25px]">
                 <p className="text-[30px] md:text-[40px] text-center md:text-left">Hey! <br /><span className="font-[700]">Welcome to PLEF! 😊</span> </p>
                 <p className="text-[20px] text-center md:text-left">What&apos;s PLEF? It&apos;s short for Piuonoshaba Life Empowerment Foundation. </p>
                 <p className="text-[20px] text-center md:text-left">The foundation is committed to empowering Nigerian youths through life to make them truly become the leaders of tomorrow! </p>
