afa7644a1931db4e247059b79afa6c3649b45c0e
Ken Udoudo <kendo302003@gmail.com>
2024-11-01 11:03:56 +0100

    our

--- src/components/About/ourstory.jsx
+++ src/components/About/ourstory.jsx
@@ -12,7 +12,7 @@ const Story = () => {
                 <img src={ rufos } className='w-[40%] bg-[#FFD4A1] absolute right-[10%] bottom-[-40%] rounded-md'/>
             </div>
             <div className='w-full lg:w-[50%] flex flex-col items-start gap-[20px]'>
-                <p className="text-[40px] font-[700]">Our Story</p>
+                <p className="text-[40px] font-[700]"><span className="italic font-[400] ">Our</span> Story</p>
                 <p className="text-[16px]">Formerly named Rufus Aid, now named after the founder, Piuonoshaba Omo Rufus, PLEF was founded with a singular vision: to invest in Nigeria’s greatest asset—its youth. It started with Mr Omo-Rufus single-handedly funding academic awards for students in the Royal Ambassadors – a worldwide Baptist missionary paramilitary organisation for boys between the ages of 10 and 24 whose vision is to touch the lives of boys and impact the eternity of men – after passing through the ranks himself.</p>
                 <p className="text-[16px]">Since the first awards distribution in 2019, to over 10 boys in the Royal Ambassadors, the number of beneficiaries and reward categories have continued to grow to accommodate more boys and their wards, who have demonstrated their commitment to excellence in academics, vocation, teaching & parenting.</p>
                 <p className="text-[16px]">The foundation was born from a deep passion for supporting academic and vocational excellence and proving its worth. We can equip the next generation with the tools and resources for youngsters to focus on excellence again. We focus on building capacity from the ground up—creating a culture of excellence and inspiring the next generation to dream big.</p>
