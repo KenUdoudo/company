479362156a529947f78f85ccdeed1658530949b0
Ken Udoudo <kendo302003@gmail.com>
2024-11-05 19:17:39 +0100

    icons

--- src/components/About/values.jsx
+++ src/components/About/values.jsx
@@ -14,35 +14,40 @@ const Values = () => {
             <div className="flex flex-col flex-wrap md:flex-row justify-between mt-[70px] gap-y-[20px] md:gap-y-[50px]">
                 <div className="flex flex-col w-full md:w-[45%]">
                     <div className="flex items-center gap-[30px]">
-                        <FaThumbsUp className="p-[25px] bg-[#d0e3ff] rounded-md"/>
+                        <div className="p-[25px] bg-[#d0e3ff] rounded-md">
+                        <FaThumbsUp className="w-[20px] h-[20px]"/></div>
                         <p className="text-[25px] font-[700]">Excellence</p>
                     </div>
                     <p className="mt-[20px]">We are committed to fostering a culture of academic and vocational excellence.</p>
                 </div>
                 <div className="flex flex-col w-full md:w-[45%]">
                     <div className="flex items-center gap-[30px]">
-                        <GiStumpRegrowth className="p-[25px] bg-[#ffd7e3] rounded-md"/>
+                        <div className="p-[25px] bg-[#ffd7e3] rounded-md">
+                        <GiStumpRegrowth className="w-[20px] h-[20px]"/></div>
                         <p className="text-[25px] font-[700]">Empowerment</p>
                     </div>
                     <p className="mt-[20px]">We believe in empowering youth to unlock their full potential.</p>
                 </div>
                 <div className="flex flex-col w-full md:w-[45%]">
                     <div className="flex items-center gap-[30px]">
-                        <IoMdBuild className="p-[25px] bg-[#ead8fc] rounded-md"/>
+                        <div className="p-[25px] bg-[#ead8fc] rounded-md">
+                        <IoMdBuild  className="w-[20px] h-[20px]"/></div>
                         <p className="text-[25px] font-[700]">Innovation</p>
                     </div>
                     <p className="mt-[20px]">We encourage creativity, innovation, and entrepreneurial thinking in all our programs.</p>
                 </div>
                 <div className="flex flex-col w-full md:w-[45%]">
                     <div className="flex items-center gap-[30px]">
-                        <TbHeartHandshake className="p-[25px] bg-[#e7f8c7] rounded-md"/>
+                        <div className="p-[25px] bg-[#e7f8c7] rounded-md">
+                        <TbHeartHandshake  className="w-[20px] h-[20px]"/></div>
                         <p className="text-[25px] font-[700]">Partnership</p>
                     </div>
                     <p className="mt-[20px]">We build strong collaborations with communities, schools, businesses, and other stakeholders to support youth development.</p>
                 </div>
                 <div className="flex flex-col w-full md:w-[45%]">
                     <div className="flex items-center gap-[30px]">
-                        <HiSparkles className="p-[25px] bg-[#d0e3ff] rounded-md"/>
+                        <div className="p-[25px] bg-[#d0e3ff] rounded-md">
+                        <HiSparkles  className="w-[20px] h-[20px]"/></div>
                         <p className="text-[25px] font-[700]">Integrity</p>
                     </div>
                     <p className="mt-[20px]">We uphold the highest standards of transparency, fairness, and accountability in our operations.</p>
