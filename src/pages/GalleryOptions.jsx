8c2cc93de80843d2d2b462907d1478c01a72be91
Ken Udoudo <kendo302003@gmail.com>
2025-04-03 19:54:39 +0100

    working

--- src/pages/GalleryOptions.jsx
+++ src/pages/GalleryOptions.jsx
@@ -1,10 +1,12 @@
+import { Link } from "react-router-dom";
+
 const GalleryOptions = () => {
     return (
         <div className="mt-[20px]">
             <p className="text-[40px] font-[700]">Gallery</p>
 
             <div className="flex flex-col w-full px-[20px] mt-[30px] gap-[20px]">
-                <p className="border-4 pl-[10px] text-[19px] border-[#F7941D] font-[600] py-[10px] hover:bg-[#F7941D] hover:text-white">Christmas Program Photos</p>
+                <Link to="/christmasprogramphotos"> <p className="border-4 pl-[10px] text-[19px] border-[#F7941D] font-[600] py-[10px] hover:bg-[#F7941D] hover:text-white">Christmas Program Photos</p></Link>
             </div>
         </div>
     );
