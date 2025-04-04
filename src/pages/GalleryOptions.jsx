ffc231e002454a56fb52364741d0623b4d590e89
Ken Udoudo <kendo302003@gmail.com>
2025-04-04 15:20:38 +0100

    about us

--- src/pages/GalleryOptions.jsx
+++ src/pages/GalleryOptions.jsx
@@ -6,7 +6,7 @@ const GalleryOptions = () => {
             <p className="text-[40px] font-[700]">Gallery</p>
 
             <div className="flex flex-col w-full px-[20px] mt-[30px] gap-[20px]">
-                <Link to="/christmasprogramphotos"> <p className="border-4 pl-[10px] text-[19px] border-[#F7941D] font-[600] py-[10px] hover:bg-[#F7941D] hover:text-white">Christmas Program Photos</p></Link>
+                <Link to="/christmasprogramphotos"> <p className="border-4 pl-[10px] text-[19px] border-[#F7941D] font-[600] py-[10px] hover:bg-[#F7941D] hover:text-white">Christmas Program</p></Link>
             </div>
         </div>
     );
