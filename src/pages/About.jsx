403887a34cb8f67a2ff8562801f9e48e50860173
Ken Udoudo <kendo302003@gmail.com>
2025-04-04 13:32:43 +0100

    done

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -4,6 +4,7 @@ import Mission from "../components/About/mission";
 import Story from "../components/About/ourstory";
 import { useEffect } from "react";
 import Values from "../components/About/values";
+import { Link } from "react-router-dom";
 const About = () => {
     useEffect(() => {
         window.scrollTo(0, 0);
@@ -19,6 +20,10 @@ const About = () => {
             <Metrics />
 
             <Values />
+
+            <Link to="/gallery" className="flex w-full justify-center items-center mt-[20px] md:mt-[40px]">
+                <p className="text-white bg-[#F7941D] hover:bg-[#f7951d98] font-[700] p-[20px] rounded-xl">See Gallery</p>
+            </Link>
         </div>
     );
 }
