40414f6d229ef45830351fdf8a0662fd82253280
Ken Udoudo <kendo302003@gmail.com>
2025-04-09 15:53:52 +0100

    test

--- src/components/gallery/firstgallery.jsx
+++ src/components/gallery/firstgallery.jsx
@@ -4,7 +4,7 @@ import "react-image-lightbox/style.css";
 
 const images = [
     "/christmasphotos/image (1).jpg",
-    "/christmasphotos/image (2).jpg",
+    "/christmasphotos/image_(2).jpg",
     "/christmasphotos/image (3).jpg",
     "/christmasphotos/image (4).jpg",
     "/christmasphotos/image (5).jpg",
