6be3efb890423a3562f59fbc18d3ae952817e654
KenUdoudo <101623697+KenUdoudo@users.noreply.github.com>
2025-08-10 04:25:51 +0100

    Update About.jsx

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,6 +2,7 @@ import Intro from "../components/About/intro";
 import Metrics from "../components/About/metrics";
 import Mission from "../components/About/mission";
 import Story from "../components/About/ourstory";
+import Poster from "./IMG_4408.png"
 import { useEffect } from "react";
 import Values from "../components/About/values";
 import { Link } from "react-router-dom";
@@ -20,6 +21,10 @@ const About = () => {
             <Metrics />
 
             <Values />
+
+            <div className="flex w-full justify-center items-center mt-[30px] px-[20px]">
+                 <img src={ Poster } />
+            </div>
 
             <Link to="/gallery" className="flex w-full justify-center items-center mt-[20px] md:mt-[40px]">
                 <p className="text-white bg-[#F7941D] hover:bg-[#f7951d98] font-[700] p-[20px] rounded-xl">See Gallery</p>
